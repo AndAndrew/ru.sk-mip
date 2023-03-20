13924bfdbf23a06941dab56637027837fa6cc90e
andAndrew <andAndrew.k@gmail.com>
2023-03-20 15:44:28 +0400

    fix: fixed Partners block text

--- src/Candidates/Candidates.tsx
+++ src/Candidates/Candidates.tsx
@@ -2,9 +2,9 @@ import styles from './Candidates.module.scss'
 export const Candidates = () => {
   return (
     <div id={'Candidates'} className={styles.candidates}>
-        <h2>Партнерам</h2>
-        <div>информация для партнеров</div>
-        <div>форма для обратной связи</div>
+        <h2>Если Вы в поиске работы</h2>
+        <div>информация для кандидатов</div>
+        <div>заполните анкету</div>
     </div>
   );
 }
