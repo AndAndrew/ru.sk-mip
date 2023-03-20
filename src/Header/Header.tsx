9fdcf1a4c97c703071432a4cfc69535893a81550
andAndrew <andAndrew.k@gmail.com>
2023-03-20 16:12:02 +0400

    feat: added logo block in Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
+
 export const Header = () => {
-  return (
-    <div className={styles.header}>
-      <Nav/>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}/>
+            <Nav/>
+        </div>
+    );
 }
