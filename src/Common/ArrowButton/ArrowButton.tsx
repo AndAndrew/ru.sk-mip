1fc26d6978caad274abb49744e82668734ab9905
andAndrew <andAndrew.k@gmail.com>
2023-05-05 12:17:13 +0400

    fix: fixed sort arrow hover color

--- src/Common/ArrowButton/ArrowButton.tsx
+++ src/Common/ArrowButton/ArrowButton.tsx
@@ -1,5 +1,3 @@
-import arrowUp from '../../Assets/arrowUp.svg'
-import arrowDown from '../../Assets/arrowDown.svg'
 import styles from './ArrowButton.module.scss'
 
 type PropsType = {
@@ -10,7 +8,32 @@ type PropsType = {
 export const ArrowButton = (props: PropsType) => {
     return (
         props.type === 'up'
-            ? <img className={styles.img} src={arrowUp} onClick={props.onClickHandler} alt={'по возрастанию'}/>
-            : <img className={styles.img} src={arrowDown} onClick={props.onClickHandler} alt={'по убыванию'}/>
+            ?
+            <svg className={styles.img}
+                 onClick={props.onClickHandler}
+                 width="512"
+                 height="512"
+                 viewBox="0 0 512 512"
+                 fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path className=""
+                      d="M419.5 337.5L256.5 174.5L93.5 337.5"
+                      stroke="black"
+                      stroke-width="24"
+                      stroke-linecap="round"/>
+            </svg>
+            :
+            <svg className={styles.img}
+                 onClick={props.onClickHandler}
+                 width="512"
+                 height="512"
+                 viewBox="0 0 512 512"
+                 fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path d="M93.5 174.5L256.5 337.5L419.5 174.5"
+                      stroke="black"
+                      stroke-width="24"
+                      stroke-linecap="round"/>
+            </svg>
     );
 };
